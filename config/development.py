9d45b6987455c4f63099cb676b59ad7a72520ef1
OJW95 <103932764+OJW95@users.noreply.github.com>
2022-06-08 03:55:46 +0900

    Update development.py

--- config/development.py
+++ config/development.py
@@ -1,9 +1,9 @@
 from config.default import *
 
 SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'\
-    .format(user='jw',
-            pw='1234',
-            url='54.157.12.31',
-            db='postgres')
+    .format(user='',
+            pw='',
+            url='',
+            db='')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = "dev"
+SECRET_KEY = ""
